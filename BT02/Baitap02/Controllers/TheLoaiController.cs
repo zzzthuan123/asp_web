159f4fdab16553358cc9055f518ee0c3d2e91d0e
zzzthuan123 <1822040769@dntu.edu.vn>
2024-08-28 10:02:19 +0700

    Bai tap tren lop

---
+++ BT02/Baitap02/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap02.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
