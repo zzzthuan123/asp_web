102065fe36f6b88dede6e5ab43b94c011280ed86
zzzthuan123 <1822040769@dntu.edu.vn>
2024-09-06 14:47:00 +0700

    Bai tap 5

---
+++ BaiTap05/BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,26 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 29";
+            ViewBag.Thang = "Thang 10";
+            ViewData["Nam"] = "2024";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh thám"
+            };
+
+            return View(theloai);
+        }
+    }
+}
