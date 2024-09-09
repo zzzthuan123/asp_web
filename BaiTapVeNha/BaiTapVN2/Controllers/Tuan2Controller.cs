f3a7e00a42a7b9900d449a81cf0cda4fb773638e
zzzthuan123 <1822040769@dntu.edu.vn>
2024-09-09 10:54:18 +0700

    Bài tập về nhà

---
+++ BaiTapVeNha/BaiTapVN2/Controllers/Tuan2Controller.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha02.Controllers
+{
+    public class Tuan2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Tằng Thiên Thuận";
+            ViewBag.MSSV = "1822040769";
+            ViewData["Nam"] = "Năm 2024";
+            return View();
+        }
+        public IActionResult MayTinh(int a, int b, string pheptinh)
+        {
+            double PhepTinh;
+            if (pheptinh == "cong")
+                PhepTinh = a + b;
+            else if (pheptinh == "tru")
+                PhepTinh = a - b;
+            else if (pheptinh == "nhan")
+                PhepTinh = a * b;
+            else
+                PhepTinh = (double)a / b;
+            ViewBag.KetQua = PhepTinh;
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            ViewBag.HoTen = "Tằng Thiên Thuận";
+            ViewBag.MSSV = "1822040769";
+            return View();
+        }
+    }
+}
---
+++ BaiTapVeNha/BaiTapVN2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaiTapVN2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
