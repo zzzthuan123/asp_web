9b4694f6abd37205c9e9ddd2b728c2439ff29f65
zzzthuan123 <1822040769@dntu.edu.vn>
2024-10-07 16:14:38 +0700

    bài kiểm tra 03

---
+++ BaiKiemTra03_01/BaiKiemTra03_01/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BaiKiemTra03_01.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaiKiemTra03_01.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<NhanVien> NhanVien { get; set; }
+        public DbSet<PhongBan> PhongBan { get; set; }
+    }
+}
---
+++ BaiKiemTra03_01/BaiKiemTra03_01/Models/PhongBan.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra03_01.Models
+{
+    public class PhongBan
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int SoLuongnv { get; set; }
+        [Required]
+        public int PhongBanQL { get; set; }
+    }
+}
